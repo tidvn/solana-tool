fcba4466d593a1fdc2065cf6a9e6c778ab97827f
Phùng Tiến Dũng <tiendung0325@gmail.com>
2023-11-25 20:57:02 +0700

    first commit

---
+++ src/config/site.ts
@@ -0,0 +1,22 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Solana Tool",
+  description:
+    "Solana Tool",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "dashboard",
+      href: "/dashboard",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/tidvn/solana-tool",
+    docs: "https://ui.shadcn.com",
+  },
+}
